b268cfc9b81739d6639cb9e4f40257216429469a
asuetomi <a_sue@tka.att.ne.jp>
2019-09-28 16:27:17 +0900

    課題

---
+++ work_20190928-3.py
@@ -0,0 +1,58 @@
+'''
+ 1. 辞書をファイルに入れる。
+ 2. ファイルを辞書に入れる。
+ 3. 入力待ちをする。
+ 4. 入力されたものが辞書になければ「（入力された内容）って何ですか？」と質問する。
+ 5. 回答を入力待ちする。
+ 6. 質問と答えを辞書に入れ、ファイルに記録する。
+ 7. 1に戻る。
+'''
+
+dicfilename = "talkdic.csv"
+dic = {}
+
+def saveDic(filename, dic):
+    '''辞書をファイルにCSV形式で保存する
+    '''
+    with open(filename, "w", encoding="utf-8") as f:
+        for key in dic:
+            f.write(key + "," + dic[key] + "\n")
+
+def loadDic(filename):
+    '''ファイルに保存された辞書を読み出してdict型を返す
+    '''
+    rtdic ={}
+    with open(filename, "r", encoding="utf-8") as fr:
+        readdata = fr.read()
+        lines = readdata.splitlines()
+        for line in lines:
+            dic = line.split(",")
+            rtdic[dic[0]] = dic[1]
+    return rtdic
+
+# ファイルがないときのため
+with open(dicfilename, "a") as f:
+    pass
+
+#  2. ファイルを辞書に入れる。
+dic = loadDic(dicfilename)
+
+with open(dicfilename, "a", encoding="utf-8") as f:
+    while True:
+    #  3. 入力待ちをする。
+        key = input("何か言って ")
+        if key == "exit":
+            break
+        if key in dic:
+            print(dic[key]," のことですね")
+        else:
+    #  4. 入力されたものが辞書になければ「（入力された内容）って何ですか？」と質問する。
+    #  5. 回答を入力待ちする。
+            value = input(key + "って何ですか？ ")
+            print(key," は ",value," のことですね")
+    #  6. 質問と答えを辞書に入れ、ファイルに記録する。
+            dic[key] = value
+            # saveDic(dicfilename, dic)
+            f.write(key + "," + dic[key] + "\n")
+
+print("さよなら")
